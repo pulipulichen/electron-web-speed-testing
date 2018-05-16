7be8e6fd19f4bdc282e5519769f59c2044758ca7
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-05-17 02:45:29 +0800

    0517 完成發佈檔案

--- bin/build2.bat
+++ bin/build2.bat
@@ -2,5 +2,6 @@ cd %~dp0
 copy ..\config\startup-webapp.bat ..\src\webapp-wrapper-win32-x64\
 copy ..\webapp-config.json ..\src\webapp-wrapper-win32-x64\
 copy ..\autoit\startup-webapp.exe ..\src\webapp-wrapper-win32-x64\
+ren ..\src\webapp-wrapper-win32-x64\startup-webapp.exe electron-loading-test.exe 
 cd ..\src\webapp-wrapper-win32-x64\
-start /B startup-webapp.bat
+start /B electron-loading-test.exe 
