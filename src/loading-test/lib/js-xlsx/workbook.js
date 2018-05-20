db1e3a2525bc97254d6c178b92ff4f91326e0d14
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-05-21 02:04:45 +0800

    天啊，終於找到一個看起來似乎能夠用的了 http://sheetjs.com/demos/table.html 0520 設定檔儲存與載入的功能 #3

---
+++ src/loading-test/lib/js-xlsx/workbook.js
@@ -0,0 +1,150 @@
+// Support  CommonJS, AMD or independent user
+(function (name, definition) {
+  if (typeof define == 'function' && typeof define.amd == 'object') {
+    define(definition);
+  }
+  else if (typeof module != 'undefined') module.exports = definition();
+  else this[name] = definition();
+}('Workbook', function () {
+
+  // encode functions lifted from XLSX to eliminate dependency on XLSX so this can be serialized/deserialized to JSON
+  function encode_row(row) { return "" + (row + 1); }
+  function encode_col(col) { var s=""; for(++col; col; col=Math.floor((col-1)/26)) s = String.fromCharCode(((col-1)%26) + 65) + s; return s; }
+  function encode_cell(cell) { return encode_col(cell.c) + encode_row(cell.r); }
+  function encode_range(cs,ce) {
+    if(ce === undefined || typeof ce === 'number') return encode_range(cs.s, cs.e);
+    if(typeof cs !== 'string') cs = encode_cell(cs); if(typeof ce !== 'string') ce = encode_cell(ce);
+    return cs == ce ? cs : cs + ":" + ce;
+  }
+  function datenum(v, date1904) {
+    if (date1904) v += 1462;
+    return (Date.parse(v) - new Date(Date.UTC(1899, 11, 30))) / (24 * 60 * 60 * 1000);
+  }
+
+  return function Workbook(workbook) {
+
+      var ranges = {}; //track  extent of each sheet
+      var rows = {};   // accumulate data rows for each sheet
+
+      return {
+        SheetNames: [],
+        Sheets: {},
+        CellStyles: [],
+
+        getSheet: function (sheetName) {
+          if (!this.Sheets[sheetName]) {
+            this.Sheets[sheetName] = {};
+
+            this.SheetNames.push(sheetName);
+            ranges[sheetName] = {s: {c: 10000000, r: 10000000}, e: {c: 0, r: 0 }};
+            rows[sheetName] = [];
+          }
+          return this.Sheets[sheetName];
+        },
+
+        getSheetRows: function (sheetName) {
+          var ws = this.getSheet(sheetName); // init if not previously called
+          return rows[sheetName];
+        },
+
+        getRange: function (sheetName) {
+          return ranges[sheetName];
+        },
+
+        getColumnProperties: function(sheetName) {
+          var ws = this.getSheet(sheetName);
+          if (!ws['!cols']) ws['!cols'] = [];
+          return ws['!cols'];
+        },
+        setCell: function (sheetName, rowIdx, colIdx, val) {
+          var rows = this.getSheetRows(sheetName);
+          if (!rows[rowIdx]) {
+            rows[rowIdx] = [];
+          }
+          rows[rowIdx][colIdx] = val;
+          return this;
+        },
+          setColWidthChars: function(sheetName, colId, width) {
+            var cps = this.getColumnProperties(sheetName);
+            if (!cps[colId]) cps[colId] = {};
+            cps[colId].wch = width;
+          return this;
+        },
+        setColWidthPixels: function(sheetName, colId, width) {
+          var cps = this.getColumnProperties(sheetName);
+          if (!cps[colId]) cps[colId] = {};
+          cps[colId].wpx = width;
+          return this;
+        },
+        getCell: function (sheetName, rowIdx, colIdx, val) {
+          var rows = this.getSheetRows(sheetName);
+          if (!rows[rowIdx]) {
+            rows[rowIdx] = [];
+          }
+          return rows[rowIdx][colIdx] = val;
+        },
+
+
+        addRowsToSheet: function (sheetName, rows) {
+          var ws = this.getSheet(sheetName);
+          var sheetRows = this.getSheetRows(sheetName);
+          rows.forEach(function (row) {
+            sheetRows.push(row);
+          });
+          return this;
+        },
+
+        finalize: function () {
+          var self = this;
+          this.SheetNames.forEach(function (sheetName) {
+            self._finalizeSheet(sheetName);
+          });
+          return this;
+        },
+
+        // { s: { c: 0, r: 0}, e: {c: 2, r: 2}}
+        mergeCells: function (sheetName, merge) {
+          var sheet = this.getSheet(sheetName);
+          sheet["!merges"] = sheet["!merges"] || [];
+          sheet["!merges"].push(merge);
+          return this;
+        },
+
+        // data is an array of row arrays
+        // from https://gist.github.com/SheetJSDev/88a3ca3533adf389d13c
+        _finalizeSheet: function (sheetName) {
+          var ws = this.getSheet(sheetName), range = this.getRange(sheetName);
+
+          var data = this.getSheetRows(sheetName) || [];
+
+          for (var R = 0; R < data.length; ++R) {
+
+            for (var C = 0; data[R] && C < data[R].length; ++C) {
+              if (range.s.r > R) range.s.r = R;
+              if (range.s.c > C) range.s.c = C;
+              if (range.e.r < R) range.e.r = R;
+              if (range.e.c < C) range.e.c = C;
+
+              var cell = (typeof data[R][C] == 'object' ? data[R][C] : {v: data[R][C] });
+              if (cell.v == null) continue;
+              var cell_ref = encode_cell({c: C, r: R});
+
+              if (typeof cell.v === 'number') cell.t = 'n';
+              else if (typeof cell.v === 'boolean') cell.t = 'b';
+              else if (cell.v instanceof Date) {
+                cell.t = 'n';
+                cell.z = 'm/d/yy';
+                cell.v = datenum(cell.v);
+              }
+              else cell.t = 's';
+
+              ws[cell_ref] = cell;
+            }
+          }
+          if (range && range.s.c < 10000000) ws['!ref'] = encode_range(range);
+          return ws;
+        }
+      }
+    }
+}));
+
--- src/loading-test/lib/js-xlsx/xlsx-helper.js
+++ src/loading-test/lib/js-xlsx/xlsx-helper.js
@@ -1,32 +1,17 @@
 var jsono = [{//测试数据
-        "保质期临期预警(天)": "adventLifecycle",
-        "商品标题": "title",
-        "建议零售价": "defaultPrice",
+        "eng1": "adventLifecycle",
+        "eng2": "title",
+        "eng3": "defaultPrice",
+    }];
+
+var jsono_chi = [{//测试数据
+        "eng1": "adventLifecycle",
+        "eng2": "title",
+        "eng3": "defaultPrice",
         "高(cm)": "height",
         "商品描述": "Description",
         "保质期禁售(天)": "lockupLifecycle",
         "商品名称": "skuName",
-        "商品简介": "brief",
-        "宽(cm)": "width",
-        "阿达": "asdz",
-        "货号": "goodsNo",
-        "商品条码": "skuNo",
-        "商品品牌": "brand",
-        "净容积(cm^3)": "netVolume",
-        "是否保质期管理": "isShelfLifeMgmt",
-        "是否串号管理": "isSNMgmt",
-        "商品颜色": "color",
-        "尺码": "size",
-        "是否批次管理": "isBatchMgmt",
-        "商品编号": "skuCode",
-        "商品简称": "shortName",
-        "毛重(g)": "grossWeight",
-        "长(cm)": "length",
-        "英文名称": "englishName",
-        "净重(g)": "netWeight",
-        "商品分类": "categoryId",
-        "这里超过了": 1111.0,
-        "保质期(天)": "expDate"
     }];
 
 /**
@@ -36,66 +21,90 @@ var jsono = [{//测试数据
 xlsx_helper = {
     tmpDown: null, //导出的二进制对象
     download: function (filename, json, type) {
-    var tmpdata = json[0];
-    json.unshift({});
-    var keyMap = []; //获取keys
-    //keyMap =Object.keys(json[0]);
-    for (var k in tmpdata) {
-        keyMap.push(k);
-        json[0][k] = k;
-    }
-    var tmpdata = [];//用来保存转换好的json 
-    json.map((v, i) => keyMap.map((k, j) => Object.assign({}, {
-                v: v[k],
-                position: (j > 25 ? this.getCharCol(j) : String.fromCharCode(65 + j)) + (i + 1)
-            }))).reduce((prev, next) => prev.concat(next)).forEach((v, i) => tmpdata[v.position] = {
-            v: v.v
-        });
-    var outputPos = Object.keys(tmpdata); //设置区域,比如表格从A1到D10
-    var tmpWB = {
-        SheetNames: ['mySheet'], //保存的表标题
-        Sheets: {
-            'mySheet': Object.assign({},
-                    tmpdata, //内容
-                    {
-                        '!ref': outputPos[0] + ':' + outputPos[outputPos.length - 1] //设置填充区域
-                    })
+        var tmpdata = json[0];
+        json.unshift({});
+        var keyMap = []; //获取keys
+        //keyMap =Object.keys(json[0]);
+        for (var k in tmpdata) {
+            keyMap.push(k);
+            json[0][k] = k;
         }
-    };
-    tmpDown = new Blob([this.s2ab(XLSX.write(tmpWB,
-                {bookType: (type == undefined ? 'xlsx' : type), bookSST: false, type: 'binary'}//这里的数据是用来定义导出的格式类型
-        ))], {
-        type: ""
-    }); //创建二进制对象写入转换好的字节流
-    var href = URL.createObjectURL(tmpDown); //创建对象超链接
-    $("#js_xlsx_downloader").remove();
-    var _a = $('<a download="' + filename + '" id="js_xlsx_downloader">AAAAA</a>').appendTo('body');
-    _a.attr('href', href);
-    //document.getElementById("hf").href = href; //绑定a标签
-    document.getElementById("js_xlsx_downloader").click(); //模拟点击实现下载
-    setTimeout(function () { //延时释放
-        URL.revokeObjectURL(tmpDown); //用URL.revokeObjectURL()来释放这个object URL
-        //_a.remove();
-    }, 100);
-},
-getCharCol: function (n) {
-    let temCol = '',
-            s = '',
-            m = 0
-    while (n > 0) {
-        m = n % 26 + 1
-        s = String.fromCharCode(m + 64) + s
-        n = (n - m) / 26
+        var tmpdata = [];//用来保存转换好的json 
+        json.map((v, i) => keyMap.map((k, j) => Object.assign({}, {
+                    v: v[k],
+                    position: (j > 25 ? this.getCharCol(j) : String.fromCharCode(65 + j)) + (i + 1)
+                }))).reduce((prev, next) => prev.concat(next)).forEach((v, i) => tmpdata[v.position] = {
+                v: v.v
+            });
+        var outputPos = Object.keys(tmpdata); //设置区域,比如表格从A1到D10
+        var tmpWB = {
+            SheetNames: ['data'], //保存的表标题
+            Sheets: {
+                'data': Object.assign({},
+                        tmpdata, //内容
+                        {
+                            '!ref': outputPos[0] + ':' + outputPos[outputPos.length - 1] //设置填充区域
+                        })
+            }
+        };
+        
+        /* bookType can be any supported output type */
+var wopts = { bookType:'ods', bookSST:false, type:'binary' };
+
+var wbout = XLSX.write(tmpWB,wopts);
+//wbout = this.s2ab(wbout);
+
+/* the saveAs call downloads a file on the local machine */
+saveAs(new Blob([wbout],{type:"application/vnd.oasis.opendocument.spreadsheet"}), "test.ods");
+return;
+        
+        
+        
+        var _xlsx_write = XLSX.write(tmpWB,{
+                            bookType: (type === undefined ? 'xlsx' : type), 
+                            bookSST: false, 
+                            type: 'binary'
+                    });//这里的数据是用来定义导出的格式类型
+        _xlsx_write = this.s2ab(_xlsx_write);
+        tmpDown = new Blob([_xlsx_write], {
+            type: "application/octet-stream"
+        }); //创建二进制对象写入转换好的字节流
+        
+        // ---------------------------------------
+        // 準備儲存
+        
+        var href = URL.createObjectURL(tmpDown); //创建对象超链接
+        $("#js_xlsx_downloader").remove();
+        var _a = $('<a download="' + filename + '" id="js_xlsx_downloader">AAAAA</a>').appendTo('body');
+        _a.attr('href', href);
+        //document.getElementById("hf").href = href; //绑定a标签
+        document.getElementById("js_xlsx_downloader").click(); //模拟点击实现下载
+        setTimeout(function () { //延时释放
+            URL.revokeObjectURL(tmpDown); //用URL.revokeObjectURL()来释放这个object URL
+            //_a.remove();
+        }, 100);
+    },
+    getCharCol: function (n) {
+        let temCol = '',
+                s = '',
+                m = 0;
+        while (n > 0) {
+            m = n % 26 + 1;
+            s = String.fromCharCode(m + 64) + s;
+            n = (n - m) / 26;
+        }
+        return s;
+    },
+    s2ab: function (s) { //字符串转字符流
+        var buf = new ArrayBuffer(s.length);
+        var view = new Uint8Array(buf);
+        for (var i = 0; i != s.length; ++i) {
+            view[i] = s.charCodeAt(i) & 0xFF;
+        }
+        return buf;
     }
-    return s
-},
-s2ab: function(s) { //字符串转字符流
-    var buf = new ArrayBuffer(s.length);
-    var view = new Uint8Array(buf);
-    for (var i = 0; i != s.length; ++i)
-        view[i] = s.charCodeAt(i) & 0xFF;
-    return buf;
-}
 };
 
-xlsx_helper.download("檔案名稱.ods", jsono, 'ods');
+$(function () {
+    xlsx_helper.download("檔案名稱.ods", jsono, 'ods');
+});
