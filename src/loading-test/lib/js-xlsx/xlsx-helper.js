96693761cece79220727425a34058eeb428c2247
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-05-21 01:31:19 +0800

    寫入成ods不是很順利 0520 設定檔儲存與載入的功能 #3

--- src/loading-test/lib/js-xlsx/xlsx-helper.js
+++ src/loading-test/lib/js-xlsx/xlsx-helper.js
@@ -28,9 +28,14 @@ var jsono = [{//测试数据
         "这里超过了": 1111.0,
         "保质期(天)": "expDate"
     }];
-var tmpDown; //导出的二进制对象
 
-function downloadExl(json, type) {
+/**
+ * https://www.jianshu.com/p/74d405940305
+ * @type type
+ */
+xlsx_helper = {
+    tmpDown: null, //导出的二进制对象
+    download: function (filename, json, type) {
     var tmpdata = json[0];
     json.unshift({});
     var keyMap = []; //获取keys
@@ -42,7 +47,7 @@ function downloadExl(json, type) {
     var tmpdata = [];//用来保存转换好的json 
     json.map((v, i) => keyMap.map((k, j) => Object.assign({}, {
                 v: v[k],
-                position: (j > 25 ? getCharCol(j) : String.fromCharCode(65 + j)) + (i + 1)
+                position: (j > 25 ? this.getCharCol(j) : String.fromCharCode(65 + j)) + (i + 1)
             }))).reduce((prev, next) => prev.concat(next)).forEach((v, i) => tmpdata[v.position] = {
             v: v.v
         });
@@ -57,19 +62,23 @@ function downloadExl(json, type) {
                     })
         }
     };
-    tmpDown = new Blob([s2ab(XLSX.write(tmpWB,
+    tmpDown = new Blob([this.s2ab(XLSX.write(tmpWB,
                 {bookType: (type == undefined ? 'xlsx' : type), bookSST: false, type: 'binary'}//这里的数据是用来定义导出的格式类型
         ))], {
         type: ""
     }); //创建二进制对象写入转换好的字节流
     var href = URL.createObjectURL(tmpDown); //创建对象超链接
-    document.getElementById("hf").href = href; //绑定a标签
-    document.getElementById("hf").click(); //模拟点击实现下载
+    $("#js_xlsx_downloader").remove();
+    var _a = $('<a download="' + filename + '" id="js_xlsx_downloader">AAAAA</a>').appendTo('body');
+    _a.attr('href', href);
+    //document.getElementById("hf").href = href; //绑定a标签
+    document.getElementById("js_xlsx_downloader").click(); //模拟点击实现下载
     setTimeout(function () { //延时释放
         URL.revokeObjectURL(tmpDown); //用URL.revokeObjectURL()来释放这个object URL
+        //_a.remove();
     }, 100);
-}
-var getCharCol = function (n) {
+},
+getCharCol: function (n) {
     let temCol = '',
             s = '',
             m = 0
@@ -79,13 +88,14 @@ var getCharCol = function (n) {
         n = (n - m) / 26
     }
     return s
-};
-var s2ab = function(s) { //字符串转字符流
+},
+s2ab: function(s) { //字符串转字符流
     var buf = new ArrayBuffer(s.length);
     var view = new Uint8Array(buf);
     for (var i = 0; i != s.length; ++i)
         view[i] = s.charCodeAt(i) & 0xFF;
     return buf;
+}
 };
 
-downloadExl(jsono, 'ods');
+xlsx_helper.download("檔案名稱.ods", jsono, 'ods');
