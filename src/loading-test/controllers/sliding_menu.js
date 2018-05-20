76450ebd396dade9e0ae5a9a72ce87d5f9c4a063
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-05-20 22:44:23 +0800

    做了偵測按鈕的方法...很不vue 0520 缺少說明的連結 #11

--- src/loading-test/controllers/sliding_menu.js
+++ src/loading-test/controllers/sliding_menu.js
@@ -1,3 +1,35 @@
 sliding_menu = {
-    key: "sliding_menu"
+    key: "sliding_menu",
+    data: {
+        opened: false,
+        mode: 'collapse'
+    },
+    methods: {
+        modechange: function (_mode) {
+            console.log(_mode);
+            sliding_menu.data.mode = _mode;
+            var _main_content = $('ons-splitter-content#main_content');
+            if (_mode === 'split') {
+                _main_content.addClass('split');
+            }
+            else {
+                _main_content.removeClass('split');
+            }
+        },
+        open: function () {
+            sliding_menu.data.opened = true;
+        },
+        ready: function () {
+            var _mode = $('#sliding_menu').attr('mode');
+            if (_mode === undefined) {
+                setTimeout(function () {
+                    sliding_menu.methods.ready();
+                }, 0);
+            }
+            else {
+                sliding_menu.methods.modechange(_mode);
+                //console.log(_mode);
+            }
+        }
+    }
 };
