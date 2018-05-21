bbf1ebd3d9b25d74a8099d976b7095d6785e0d12
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-05-21 23:17:52 +0800

    完成，自動打包 ..? close #29 0520 做一個打包的工具 #29

--- bin/package.bat
+++ bin/package.bat
@@ -1,16 +1,21 @@
 cd %~dp0
 cd ../dist
 
+del "..\src\webapp-wrapper-win32-x64\resources\app\cache\local_storage_loading_test_config.json"
+del ".\electron-loading-test-dist.7z"
+
 echo Copy webapp-wrapper-win32-x64
-xcopy /s/y/D ..\src\webapp-wrapper-win32-x64\* ..\dist\webapp-wrapper-win32-x64\*
+xcopy /s/y/D ..\src\webapp-wrapper-win32-x64\* ..\dist\electron-loading-test\webapp-wrapper-win32-x64\*
 
 echo Copy loading test.exe
-copy "..\autoit\loading test.exe" .\
+copy "..\autoit\electron-loading-test.exe" .\electron-loading-test\
 
 echo Build package
+cd ../bin/7-Zip64/
+7z.exe a -mx9 -t7z ../../dist/electron-loading-test-dist.7z "../../dist/electron-loading-test"
+cd ../../dist
 
 echo Delete files
-rmdir /S /Q .\webapp-wrapper-win32-x64
-del ".\loading test.exe"
+rmdir /S /Q .\electron-loading-test
 
-pause
+start "" https://drive.google.com/open?id=1w_fEdw5w5etIYKPDI4PcGn3tYEHipr9e
