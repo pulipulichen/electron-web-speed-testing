5c9b286463fda9ea3ef5d109686ba69e6c6f0563
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-05-21 22:45:24 +0800

    來試試看？ 0520 做一個打包的工具 #29

--- bin/package.bat
+++ bin/package.bat
@@ -2,11 +2,15 @@ cd %~dp0
 cd ../dist
 
 echo Copy webapp-wrapper-win32-x64
+xcopy /s/y ..\src\webapp-wrapper-win32-x64 .\webapp-wrapper-win32-x64
 
 echo Copy loading test.exe
+xcopy /s/y "..\autoit\loading test.exe" ".\loading test.exe"
 
 echo Build package
 
 echo Delete files
+rmdir /S /Q .\webapp-wrapper-win32-x64
+del ".\loading test.exe"
 
 pause
